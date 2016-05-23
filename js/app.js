7ca72bad768adadd9e6719df80321f4f5ad353a3
Awilda <awildacantres@gmail.com>
2016-05-22 22:41:17 -0400

    Add highlight function and refactored code

--- js/app.js
+++ js/app.js
@@ -1,48 +1,55 @@
 var spreadsheet, cell, numbers;
+var letters = ["A","B","C","D","E","F","G"];
+var numbers = [1,2,3,4,5,6,7];
 
- function addCells() {
+function addCells() {
  	var table = document.getElementById("myTable");
  	var row = table.insertRow(0);
  		for(var l = 0; l < 8; l++){
  			cell = row.insertCell(0);
  		}
- };
-
- function newSheet(){
-  for(var x = 0; x < 8; x++){
- 		spreadsheet = new addCells();
-  }
- 	var letterA = document.getElementById('myTable').rows[0].cells[1];
- 		letterA.innerHTML="A";
- 	var letterB = document.getElementById('myTable').rows[0].cells[2];
- 		letterB.innerHTML="B";
- 	var letterC = document.getElementById('myTable').rows[0].cells[3];
- 		letterC.innerHTML="C";
- 	var letterD = document.getElementById('myTable').rows[0].cells[4];
- 		letterD.innerHTML="D";
- 	var letterE = document.getElementById('myTable').rows[0].cells[5];
- 		letterE.innerHTML="E";
- 	var letterF = document.getElementById('myTable').rows[0].cells[6];
- 		letterF.innerHTML="F";
- 	var letterG = document.getElementById('myTable').rows[0].cells[7];
- 		letterG.innerHTML="G";
-
- 	var num1 = document.getElementById('myTable').rows[1].cells[0];
- 		num1.innerHTML="1";
- 	var numB = document.getElementById('myTable').rows[2].cells[0];
- 		numB.innerHTML="2";
- 	var numC = document.getElementById('myTable').rows[3].cells[0];
- 		numC.innerHTML="3";
- 	var numD = document.getElementById('myTable').rows[4].cells[0];
- 		numD.innerHTML="4";
- 	var numE = document.getElementById('myTable').rows[5].cells[0];
- 		numE.innerHTML="5";
- 	var numF = document.getElementById('myTable').rows[6].cells[0];
- 		numF.innerHTML="6";
- 	var numG = document.getElementById('myTable').rows[7].cells[0];
- 		numG.innerHTML="7";
 };
 
+function highlightColumn(e) {
+	var columnLetter = e.target.className;
+  var columnNumber = letters.indexOf(columnLetter) + 1;
+
+  var table = document.getElementById("myTable");
+  for (var counter = 0; counter <= numbers.length; counter++){
+  	var column = table.rows[counter].cells[columnNumber];
+    column.classList.add("highlight");
+  }
+}
+
+function newSheet(){
+  for (var x = 0; x < 8; x++) {
+ 		spreadsheet = new addCells();
+  }
+
+	/* A-G */
+  for (var counter = 1; counter <= letters.length; counter++) {
+  	var cell = document.getElementById('myTable').rows[0].cells[counter];
+    cell.innerHTML = letters[counter-1];
+    cell.addEventListener("click", highlightColumn);
+    cell.classList.add(letters[counter-1]);
+  }
+  
+  /* 1-7 */
+  for (var index = 0; index < numbers.length; index++){
+  	var nums = document.getElementById('myTable').rows[index + 1].cells[0];
+    nums.innerHTML = numbers[index];
+  }
+  
+  /* Rest of the table */
+  for (var count = 1; count <= numbers.length; count++){
+  	for (var ind = 1; ind <= letters.length; ind++){
+  		var empt = document.getElementById('myTable').rows[count].cells[ind];
+    	empt.innerHTML = "<input />";
+  	}
+  }
+};
+
+window.onload = newSheet();
 
 
 
